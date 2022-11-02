9a1c4500af49a10626d7b68585947403e73371ec
Alexander <alexanderwuensche@hotmail.com>
2022-11-02 10:53:21 +0100

    fix code

--- Common_Functions.py
+++ Common_Functions.py
@@ -16,34 +16,60 @@ stringless_testing_data_path = "house-prices-advanced-regression-techniques/stri
 export_data_path = "Exported_Data/"
 
 def get_training_data():
+    """
+    gets the initial training data from the specified folder.
+    :return: Returns a dataframe of the training data.
+    """
     training_data = pd.read_csv(training_data_path, index_col="Id")
     return training_data
 
 
 def get_testing_data():
+    """
+    gets the initial testing data from the specified folder.
+    :return: Returns a dataframe of the testing data.
+    """
     testing_data = pd.read_csv(testing_data_path, index_col="Id")
     return testing_data
 
 
 def get_stringless_training_data():
+    """
+    gets the training data without strings, as exported from file 1.1 from the specified folder.
+    :return: Returns a dataframe of the training data.
+    """
     training_data = pd.read_csv(stringless_training_data_path)
     return training_data
 
 
 def get_stringless_testing_data():
+    """
+    gets the testing data without strings, as exported from file 1.1 from the specified folder.
+    :return: Returns a dataframe of the testing data.
+    """
     testing_data = pd.read_csv(stringless_testing_data_path)
     return testing_data
 
 
 def clean_data(data):
+    """
+    Removes columns that have strings, and replaces remaining NaN's with 0.
+    :param data: a dataframe
+    :return: a cleaned version of the dataframe. Is mainly used in notebook 1.1 for initial cleaning.
+    """
     column_to_remove = list(data.select_dtypes(include=['category', 'object']))
     cleaned_data = data.drop(column_to_remove, axis=1)
     cleaned_data = cleaned_data.replace({np.NaN: 0})
-
     return cleaned_data
 
 
 def merge_predicted_price(data, prediction):
+    """
+    Merges the predicted price onto the X_test dataframe so that it can be repassed through the training models.
+    :param data: a dataframe.
+    :param prediction: a dataframe with the predicted price.
+    :return: reuturns the dataframe data with an additional column.
+    """
     predicted_list = pd.DataFrame(prediction, columns=['SalePrice'])
     merged_data = data.reset_index()
     merged_data["SalePrice"] = predicted_list
@@ -53,6 +79,14 @@ def merge_predicted_price(data, prediction):
 
 
 def plot_matrix(data, threshold, is_saved=False, filename=""):
+    """
+    Plots a correlation matrix.
+    :param data: a dataframe
+    :param threshold: a value between 0 and 1 to reduce the amount of entries in the matrix
+    :param is_saved: True or False, saves the matrix as a png
+    :param filename: string, changes the file name of the exported png
+    :return: a correlation trimmed correlation matrix with values only above the threshold
+    """
     matrix = data.corr().abs()
     columns_to_keep = matrix.iloc[-1, :] >= threshold
     rows_to_keep = matrix.iloc[:, -1] >= threshold
@@ -66,9 +100,20 @@ def plot_matrix(data, threshold, is_saved=False, filename=""):
     return new_matrix
 
 
-def plot_feature_importance(x_val, forest, x_lim=20, y_lim=0.6, is_saved=False, filename="", fig_size=(20,10)):
+def plot_feature_importance(x_val, regressor, x_lim=20, y_lim=0.6, is_saved=False, filename="", fig_size=(20, 10)):
+    """
+    Bar chart that plots the feature importance as dictated by the regression function
+    :param x_val:
+    :param regressor:
+    :param x_lim:
+    :param y_lim:
+    :param is_saved:
+    :param filename:
+    :param fig_size:
+    :return:
+    """
     feature_names = [x for x in x_val.columns]
-    importance = forest.feature_importances_
+    importance = regressor.feature_importances_
     sorted_lists = sorted(zip(importance, feature_names), reverse=True)
     importance, feature_names = [[x[0] for x in sorted_lists], [x[1] for x in sorted_lists]]
     plt.figure(figsize=fig_size)
@@ -84,6 +129,18 @@ def plot_feature_importance(x_val, forest, x_lim=20, y_lim=0.6, is_saved=False, 
 
 
 def plot_multi_scatter(columns, y_feature_name, data, is_saved=False, filename="", column_len=3, row_len=2, fig_size=(20,10)):
+    """
+
+    :param columns:
+    :param y_feature_name:
+    :param data:
+    :param is_saved:
+    :param filename:
+    :param column_len:
+    :param row_len:
+    :param fig_size:
+    :return:
+    """
     min_max_scaler = preprocessing.MinMaxScaler()
     #column_len = math.ceil(len(columns) / 2)
     x = data.loc[:, columns]
@@ -100,6 +157,7 @@ def plot_multi_scatter(columns, y_feature_name, data, is_saved=False, filename="
 
 
 def plot_observed_vs_predicted(observed=None, predicted=None, x_lim=100, y_lim=500000, is_saved=False, filename="", size_of_fig=None):
+
     fig, ax = plt.subplots(figsize=size_of_fig)
     if observed is not None:
         plt.plot(observed, alpha=0.8, label=r"Observed Price")
